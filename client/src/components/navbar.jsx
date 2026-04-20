2f354feb902d9b7d3c7810d83ea89e335643b7c2
juanAmprimo <amprimojuangabrielgmail.com>
2026-04-20 18:26:01 -0300

    navbar cambios 2

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,38 +1,47 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import logo from './assets/logo-hustlery.jpeg';               
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+    <nav className="navbar navbar-expand-lg navbar-light custom-bg fixed-top">
       <div className="container-fluid">
-        {/* Marca */}
-        <a className="navbar-brand fw-bold" href="#">CapStyle</a>
-        {/* Botón hamburguesa para móviles */}
+        {/* Marca con logo */}
+        <a className="navbar-brand fw-bold" href="#">
+          <img 
+            src={logo} 
+            alt="CapStyle Logo" 
+            height="40" 
+            className="d-inline-block align-text-top"
+          />
+        </a>
+
+        {/* Botón hamburguesa */}
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
           <span className="navbar-toggler-icon"></span>
         </button>
 
-        {/* Links de navegación */}
+        {/* Links */}
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav mx-auto">
             <li className="nav-item">
-              <a className="nav-link active" href="#">Inicio</a>
+              <a className="nav-link hover-nav active" href="#">Inicio</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Nosotros</a>
+              <a className="nav-link hover-nav" href="#">Nosotros</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Contacto</a>
+              <a className="nav-link hover-nav" href="#">Contacto</a>
             </li>
           </ul>
 
           {/* Íconos a la derecha */}
           <div className="d-flex">
-            <a href="#" className="nav-link">
-              <i className="bi bi-cart"></i> {/* carrito */}
+            <a href="#" className="nav-link hover-nav">
+              <i className="bi bi-cart"></i>
             </a>
-            <a href="#" className="nav-link">
-              <i className="bi bi-person"></i> {/* perfil */}
+            <a href="#" className="nav-link hover-nav">
+              <i className="bi bi-person"></i>
             </a>
           </div>
         </div>

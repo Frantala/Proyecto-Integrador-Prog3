9f4fcbae1547a9fceb8ff428a0c2f4b9c86ab2d7
Franco Talarico <francotalarico01@gmail.com>
2026-04-21 17:49:59 -0300

    Importando el componente ProductCard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css';
 import './index.css';
 import Navbar from './components/Navbar';
-
+import { ProductCard } from './components/ProductCard';
 function App() {
   
 
   return (
     <>
       <Navbar/>
+      <ProductCard />
     </>
   )
 }

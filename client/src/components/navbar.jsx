70889874d407e868062acb3633cb6f523e52a055
juanAmprimo <amprimojuangabrielgmail.com>
2026-04-21 18:38:46 -0300

    logo en el navbar agregado

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,16 +1,28 @@
 import { Navbar as BootstrapNavbar, Container, Nav } from 'react-bootstrap';
+import logo from '../assets/logo-hustlery.png'; 
 
 const CustomNavbar = () => {
   return (
     <BootstrapNavbar expand="lg" style={{ backgroundColor: '#c1f0f6' }} className="px-4 py-3">
       <Container fluid>
-        <BootstrapNavbar.Brand href="#" className="fw-bold fs-3">CapStyle</BootstrapNavbar.Brand>
+        <BootstrapNavbar.Brand href="#" className="fw-bold fs-3 d-flex align-items-center">
+          {/* Logo */}
+          <img
+            src={logo}
+            alt="CapStyle Logo"
+            height="40"
+            className="me-2 d-inline-block align-text-top"
+          />
+          {/* Texto */}
+          Hustlery
+        </BootstrapNavbar.Brand>
+
         <BootstrapNavbar.Toggle aria-controls="navbar-nav" />
         <BootstrapNavbar.Collapse id="navbar-nav">
           <Nav className="mx-auto fw-medium">
-            <Nav.Link href="#">Inicio</Nav.Link>
-            <Nav.Link href="#">Nosotros</Nav.Link>
-            <Nav.Link href="#">Contacto</Nav.Link>
+            <Nav.Link href="#">Home</Nav.Link>
+            <Nav.Link href="#">About Us</Nav.Link>
+            <Nav.Link href="#">Contact</Nav.Link>
           </Nav>
           <Nav className="gap-3 align-items-center">
             <Nav.Link href="#" style={{ fontSize: '1.2rem' }}>🛒</Nav.Link>

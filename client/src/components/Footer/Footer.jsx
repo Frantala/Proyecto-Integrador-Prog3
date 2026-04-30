3a8c7ad290b91b0a50cf08f51374e64ae1ce424e
juanAmprimo <amprimojuangabrielgmail.com>
2026-04-30 09:26:43 -0300

    cambio de tamaño en el footer con clase en css

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -3,17 +3,14 @@ import { FaInstagram } from "react-icons/fa";
 
 function Footer() {
   return (
-    <footer style={{ backgroundColor: "#c1f0f6" }} className="mt-5 py-3 shadow-sm">
+    <footer className="footer-custom shadow-sm" style={{ backgroundColor: "#c1f0f6" }}>
       <Container className="h-100">
         <Row className="align-items-center h-100">
-          {/* Texto */}
           <Col xs={12} sm={6} className="text-center text-sm-start">
             <p className="mb-0 text-dark">
               © 2026 Hustlery. Todos los derechos reservados.
             </p>
           </Col>
-
-          {/* Redes */}
           <Col xs={12} sm={6} className="d-flex justify-content-center justify-content-sm-end">
             <a
               href="https://www.instagram.com/hustlery__"

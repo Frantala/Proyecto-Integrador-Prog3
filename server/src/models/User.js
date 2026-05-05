8489ab112d8d1788ea07236daebe4cf888cbba9a
juanAmprimo <amprimojuangabrielgmail.com>
2026-05-05 08:43:31 -0300

    cambios navbar

---
+++ server/src/models/User.js
@@ -0,0 +1,32 @@
+// este modelo gestiona la autenticacion y los trs roles obligatorios
+import { DataTypes } from "sequelize";
+import { sequelize } from "../database/db.js";
+
+export const User = sequelize.define('User', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {isEmail: true} //validacion de formato email
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    role: {
+        type: DataTypes.STRING,
+        defaultValue: 'usuario',
+        validate: {
+            isIn: [['super-admin', 'admin', 'usuario']] //roles requeridos
+        }
+    }
+});

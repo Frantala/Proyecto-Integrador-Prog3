a93614e82fe62f6ce1f411110fd68daa41134ca1
Franco Talarico <francotalarico01@gmail.com>
2026-05-05 09:53:59 -0300

    intento de arreglar el navbar de la aplicacion

--- client/src/components/Navbar/navbar.jsx
+++ client/src/components/Navbar/navbar.jsx
@@ -1,23 +1,16 @@
 import { Navbar as BootstrapNavbar, Container, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import logo from "../../assets/logo-hustlery.png";
-import "./navbar.css";
+import "./Navbar.css";
+
 
 const CustomNavbar = () => {
   return (
-    <BootstrapNavbar 
-      expand="lg"                 
-      className="navbar-custom shadow-sm" 
-      style={{ backgroundColor: "#c1f0f6" }}
-    >
-      <Container fluid>
+    <BootstrapNavbar className="navbar-custom shadow-sm" style={{ backgroundColor: "#c1f0f6" }}>
+      <Container fluid className="d-flex justify-content-between align-items-center">
         
         {/* Brand */}
-        <BootstrapNavbar.Brand 
-          as={Link} 
-          to="/" 
-          className="brand-custom d-flex align-items-center"
-        >
+        <BootstrapNavbar.Brand as={Link} to="/" className="brand-custom d-flex align-items-center">
           <img
             src={logo}
             alt="Hustlery Logo"
@@ -26,21 +19,17 @@ const CustomNavbar = () => {
           <span className="brand-text">Hustlery</span>
         </BootstrapNavbar.Brand>
 
-        {/* Botón hamburguesa */}
-        <BootstrapNavbar.Toggle aria-controls="basic-navbar-nav" />
+        {/* Links */}
+        <Nav className="nav-links mx-auto fw-medium">
+          <Nav.Link as={Link} to="/">Inicio</Nav.Link>
+          <Nav.Link as={Link} to="/about-us">Sobre Nosotros</Nav.Link>
+        </Nav>
 
-        {/* Contenido colapsable */}
-        <BootstrapNavbar.Collapse id="basic-navbar-nav">
-          <Nav className="mx-auto fw-medium">
-            <Nav.Link as={Link} to="/">Inicio</Nav.Link>
-            <Nav.Link as={Link} to="/about-us">Sobre Nosotros</Nav.Link>
-          </Nav>
-
-          <Nav className="nav-icons align-items-center">
-            <Nav.Link as={Link} to="/cart">🛒</Nav.Link>
-            <Nav.Link as={Link} to="/login">👤</Nav.Link>
-          </Nav>
-        </BootstrapNavbar.Collapse>
+        {/* Icons */}
+        <Nav className="nav-icons align-items-center">
+          <Nav.Link as={Link} to="/cart">🛒</Nav.Link>
+          <Nav.Link as={Link} to="/login">👤</Nav.Link>
+        </Nav>
       </Container>
     </BootstrapNavbar>
   );

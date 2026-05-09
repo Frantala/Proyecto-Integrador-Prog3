6238a3e500e63c59ed15184b79ce97b46037c3ea
Franco Talarico <francotalarico01@gmail.com>
2026-05-09 14:37:53 -0300

    mejora del navbar en responsive y ahora queda fijo el navbar mientras se escrolea

--- client/src/components/Navbar/navbar.jsx
+++ client/src/components/Navbar/navbar.jsx
@@ -6,30 +6,37 @@ import "./Navbar.css";
 
 const CustomNavbar = () => {
   return (
-    <BootstrapNavbar className="navbar-custom shadow-sm" style={{ backgroundColor: "#c1f0f6" }}>
-      <Container fluid className="d-flex justify-content-between align-items-center">
+    <BootstrapNavbar expand="lg" className="navbar-custom shadow-sm" style={{ backgroundColor: "#c1f0f6" }} sticky="top">
+      <Container fluid>
         
         {/* Brand */}
         <BootstrapNavbar.Brand as={Link} to="/" className="brand-custom d-flex align-items-center">
           <img
             src={logo}
             alt="Hustlery Logo"
-            className="logo-navbar me-2 d-inline-block align-text-top"
+            className="logo-navbar d-inline-block align-text-top"
           />
           <span className="brand-text">Hustlery</span>
         </BootstrapNavbar.Brand>
 
-        {/* Links */}
-        <Nav className="nav-links mx-auto fw-medium">
-          <Nav.Link as={Link} to="/">Inicio</Nav.Link>
-          <Nav.Link as={Link} to="/about-us">Sobre Nosotros</Nav.Link>
-        </Nav>
+        {/* Toggle para menú hamburguesa en móvil */}
+        <BootstrapNavbar.Toggle aria-controls="basic-navbar-nav" className="navbar-toggler-custom" />
 
-        {/* Icons */}
-        <Nav className="nav-icons align-items-center">
-          <Nav.Link as={Link} to="/cart">🛒</Nav.Link>
-          <Nav.Link as={Link} to="/login">👤</Nav.Link>
-        </Nav>
+        {/* Contenedor colapsable */}
+        <BootstrapNavbar.Collapse id="basic-navbar-nav" className="navbar-collapse-custom">
+          
+          {/* Links - Centered */}
+          <Nav className="nav-links fw-medium">
+            <Nav.Link as={Link} to="/">Inicio</Nav.Link>
+            <Nav.Link as={Link} to="/about-us">Sobre Nosotros</Nav.Link>
+          </Nav>
+
+          {/* Icons - Right */}
+          <Nav className="nav-icons align-items-center">
+            <Nav.Link as={Link} to="/cart">🛒</Nav.Link>
+            <Nav.Link as={Link} to="/login">👤</Nav.Link>
+          </Nav>
+        </BootstrapNavbar.Collapse>
       </Container>
     </BootstrapNavbar>
   );
